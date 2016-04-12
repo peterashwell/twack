9e0f5d4128a355fe3b9c498fc7551d517b996a19
peterashwell <peter.w.ashwell@gmail.com>
2016-04-11 21:13:45 -0400

    Load meta from data dir

--- Meta.py
+++ Meta.py
@@ -1,4 +1,10 @@
-with open('seed_screen_names.txt') as seed_screen_names_file:
+import os
+
+seed_screen_names_path = os.path.join(
+    'data', 'seed_screen_names.txt'
+)
+
+with open(seed_screen_names_path) as seed_screen_names_file:
     seed_screen_names = list(map(
         str.strip,
         seed_screen_names_file.readlines()
