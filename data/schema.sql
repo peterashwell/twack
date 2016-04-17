2207660e2a3b8ebfe8e805de0d22101918a02fea
peterashwell <peter.w.ashwell@gmail.com>
2016-04-17 12:21:17 -0400

    Fix tabs/spaces in schema

--- data/schema.sql
+++ data/schema.sql
@@ -1,30 +1,34 @@
 create table twitter_user (
-	id integer primary key,
-	user_id string,
-	screen_name string,
-	followers_count integer,
-	friends_count integer,
-	blob string
+    id integer primary key,
+    user_id string,
+    screen_name string,
+    followers_count integer,
+    friends_count integer,
+    blob string,
+    unique(user_id)
 );
 
 create table seed_followers (
-	id integer primary key,
-	user_id integer,
-	follower_of_screen_name string
+    id integer primary key,
+    user_id integer,
+    follower_of_screen_name string,
+    unique(user_id, follower_of_screen_name)
 );
 
 create table follow_attempts (
-	id integer primary key,
+    id integer primary key,
     user_id integer,
-	attempt_date string
+    attempt_date string
 );
 
 create table my_followers (
-	id integer primary_key,
-	user_id integer
+    id integer primary_key,
+    user_id integer,
+    unique(user_id)
 );
 
 create table my_friends (
-	id integer primary_key,
-	user_id integer
+    id integer primary_key,
+    user_id integer,
+    unique(user_id)
 );
