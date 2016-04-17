8ff70fe4a2b4115af69aee330e8cc50ccf18a735
peterashwell <peter.w.ashwell@gmail.com>
2016-04-16 21:52:32 -0400

    Fix add influencer script

--- follow_influencers.py
+++ follow_influencers.py
@@ -17,8 +17,11 @@ my_follower_ids = set(my_status.load_followers())
 influencers = analyser.get_sorted_followers()
 
 for influencer in influencers:
-    user_id = influencer['id_str']
-    if user_id not in my_follower_ids:
-        tweepy_with_auth.create_friendship(user_id)
-        print('script:follow_influencers | following {0}'.format(influencer['screen_name']))
-        time.sleep(TWITTER_CREATE_FRIENDSHIP_API_REQUEST_SPACING_SECONDS)
+    try:
+        user_id = influencer['id_str']
+        if user_id not in my_follower_ids:
+            tweepy_with_auth.create_friendship(user_id)
+            print('script:follow_influencers | following {0}'.format(influencer['screen_name']))
+            time.sleep(TWITTER_CREATE_FRIENDSHIP_API_REQUEST_SPACING_SECONDS)
+    except Exception:
+        print('got an error lol')
