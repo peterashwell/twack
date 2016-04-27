315b9524bfabfed3c90a2167490cc6cf2f1918bb
peterashwell <peter.w.ashwell@gmail.com>
2016-04-27 16:18:08 -0400

    Fix up dump seed followers script

--- dump_seed_followers.py
+++ dump_seed_followers.py
@@ -1,15 +1,9 @@
-import sys
-
-sys.stderr = sys.stdout
-
-# Go through list of all my friends
-# Unfriend those that do not follow me
-
 from MyStatus import MyStatus
-from Auth import tweepy_with_auth
+from TwackData import TwackData
 
 td = TwackData()
 status = MyStatus()
 
+# Delete currently known seed followers, then rebuild list
 td.delete_all_seed_followers()
 status.dump_seed_followers()
