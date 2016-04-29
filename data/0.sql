f6e630de2edac59fdbf4490fadae2c26fc7f4a1c
peterashwell <peter.w.ashwell@gmail.com>
2016-04-29 11:41:20 -0400

    Fix schemas breaking into steps

--- data/0.sql
+++ data/0.sql
@@ -15,19 +15,6 @@ create table seed_followers (
     unique(user_id, follower_of_screen_name) on conflict ignore
 );
 
-create table friend_attempt (
-    id integer primary key,
-    user_id integer,
-    timestamp string default now
-);
-
-create table favorite_attempt (
-	id integer primary key,
-	user_id integer,
-	tweet_id_favorited integer,
-	timestamp string default now
-);
-
 create table my_followers (
     id integer primary_key,
     user_id integer,
---
+++ data/1.sql
@@ -0,0 +1,12 @@
+create table favorite_attempt (
+	id integer primary key,
+	user_id integer,
+	tweet_id_favorited integer,
+	timestamp string default now
+);
+
+create table friend_attempt (
+    id integer primary key,
+    user_id integer,
+    timestamp string default now
+);
