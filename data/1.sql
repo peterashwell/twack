5f266ad67c6a99267a20690dac7e47134eab7644
peterashwell <peter.w.ashwell@gmail.com>
2016-05-02 17:17:08 -0400

    Fix default timestamp in favorite/follow tracking

--- data/1.sql
+++ data/1.sql
@@ -2,11 +2,11 @@ create table favorite_attempt (
 	id integer primary key,
 	user_id integer,
 	tweet_id_favorited integer,
-	timestamp string default now
+	timestamp string default (datetime('now'))
 );
 
 create table friend_attempt (
     id integer primary key,
     user_id integer,
-    timestamp string default now
+    timestamp string default (datetime('now'))
 );
